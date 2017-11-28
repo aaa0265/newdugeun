686cec7c689698c6e9926d7d8ea718419ab2b26c
DESKTOP-6R34HE7\Jiwon <jiwon72674@gmail.com>
2017-11-28 16:18:25 +0900

    모든 코드 업로드함.

---
+++ app/src/main/java/com/example/kmucs/dugeun/exchange_current.java
@@ -0,0 +1,17 @@
+package com.example.kmucs.dugeun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by blue8 on 2017-10-30.
+ */
+
+public class exchange_current extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_current);
+    }
+}
---
+++ app/src/main/java/com/example/kmucs/dugeun/exchange_exchanged.java
@@ -0,0 +1,16 @@
+package com.example.kmucs.dugeun;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by blue8 on 2017-10-31.
+ */
+
+public class exchange_exchanged extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exchange_exchanged);
+    }
+}
