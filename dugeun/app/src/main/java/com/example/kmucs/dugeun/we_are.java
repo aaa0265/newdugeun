e58330636e0d9168e3ec0edce3aae04c2ba5a3c1
DESKTOP-6R34HE7\Jiwon <jiwon72674@gmail.com>
2017-11-29 19:58:58 +0900

    가계부, 지도, info 주석 추가함.

--- dugeun/app/src/main/java/com/example/kmucs/dugeun/we_are.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/we_are.java
@@ -10,10 +10,14 @@ public class we_are extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //we_are의 레이아웃 가져옴
         setContentView(R.layout.activity_we_are);
     }
 
+    //버튼 onclick
     public void info(View v){
+        // info로 넘어가기
+        // getApplicationContext : 현재 액티비티 정보를 가지고 있음
         Intent intent = new Intent(getApplicationContext(), info2.class);
         startActivity(intent);
     }
