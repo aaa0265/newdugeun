a4e4ca674d79369d8485eafaee6b98f8bf08aca1
DESKTOP-6R34HE7\Jiwon <jiwon72674@gmail.com>
2017-11-30 16:13:52 +0900

    to do list 주석추가함.

--- dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo.java
@@ -23,6 +23,7 @@ public class onememo extends SQLiteOpenHelper{
         super(context, DB_NAME, null, DB_VER);
     }
 
+    //테이블 생성
     @Override
     public void onCreate (SQLiteDatabase db){
         String query = String.format("CREATE TABLE %s (ID INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT NOT NULL);", DB_TABLE, DB_COLUMN);
@@ -37,25 +38,40 @@ public class onememo extends SQLiteOpenHelper{
         onCreate(db);
     }
 
+    //내용 삽입 함수
     public void insertNewTask(String task){
+        //쓰기가능으로 열기
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(DB_COLUMN, task);
+        //------------------ 모름
         db.insertWithOnConflict(DB_TABLE,null,values,SQLiteDatabase.CONFLICT_REPLACE);
         db.close();
     }
 
+    //삭제 함수
     public void deleteTask(String task){
         SQLiteDatabase db = this.getWritableDatabase();
+        //중복된 값도 삭제됨.
         db.delete(DB_TABLE,DB_COLUMN + " = ?",new String[]{task});
         db.close();
     }
 
+    //task table에 있는 메모들을 가져온다.
     public ArrayList<String> getTaskList(){
             ArrayList<String> taskList = new ArrayList<>();
+            //읽기가능한 형태로 db불러오기
             SQLiteDatabase db = this.getReadableDatabase();
+
+        /** query() 메소드사용
+         1. 대상 테이블 이름
+         2. 내가 가져올 데이터가 있는 칼럼의 배열
+         3~7 : 필요없는 부분이라 null
+         가져온 애들을 cursor에 넣는다. */
+
         Cursor cursor = db.query(DB_TABLE, new String[]{DB_COLUMN},null,null,null,null,null);
         while (cursor.moveToNext()){
+            //getColumnIndex(DB_COLUMN) = 1번째 열 = taskname
             int index = cursor.getColumnIndex(DB_COLUMN);
             taskList.add(cursor.getString(index));
         }
--- dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo_main.java
+++ dugeun/app/src/main/java/com/example/kmucs/dugeun/onememo_main.java
@@ -21,30 +21,43 @@ import java.util.ArrayList;
  * Created by YoungeunSong on 2017-10-31.
  */
 
+// 기억해라!!!!!  ArrayAdapter<String> : 리스트 객체 안에 저장된 데이터들을 우리가 볼 수 있게 ListView 로 뿌려주는 역할.
+
+
 public class onememo_main extends AppCompatActivity {
 
+    //객체생성
     onememo onememo_;
     ArrayAdapter<String> mAdapter;
     ListView lstTask;
 
 
+    //onememo 초기화면 구성 함수
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //onememo_main 레이아웃을 가져옴
         setContentView(R.layout.activity_onememo_main);
 
         onememo_ = new onememo(this);
         lstTask = (ListView)findViewById(R.id.lstTask);
         loadTaskList();
-
     }
 
+    // list 내용을 load함.
     private void loadTaskList(){
         ArrayList<String> taskList = onememo_.getTaskList();
+        //내용이 없으면 새로 추가
         if (mAdapter==null) {
+            //this = mainactivity의 instance
+            // row = 한 줄씩, taskList = 내용 목록
             mAdapter = new ArrayAdapter<String>(this, R.layout.row, R.id.task_title, taskList);
             lstTask.setAdapter(mAdapter);
         }
+
+        // mAdapter를 초기화 한다-> .clear()
+        // mAdapter에 taskList의 내용을 모두 담는다. ->.addAll
+        // notifyDataSetChanged : 새로운 내용이 있나 확인하고 변경사항이 있으면 리스트를 갱신하라는 메소드(새로운 내용 추가됨)
         else {
             mAdapter.clear();
             mAdapter.addAll(taskList);
@@ -52,6 +65,7 @@ public class onememo_main extends AppCompatActivity {
         }
     }
 
+    //메뉴 추가 아이콘 생성
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
@@ -63,11 +77,13 @@ public class onememo_main extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    // 메모 추가를 눌렀을 때
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add_task:
                 final EditText taskEditText = new EditText(this);
+                //dialog생성하는 부분
                 AlertDialog dialog = new AlertDialog.Builder(this)
                         .setTitle("메모 추가")
                         .setMessage("무엇을 기록할까요?")
@@ -89,6 +105,7 @@ public class onememo_main extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //삭제함
     public void deleteTask(View view){
         View parent = (View)view.getParent();
         TextView taskTextView = (TextView)parent.findViewById(R.id.task_title);
